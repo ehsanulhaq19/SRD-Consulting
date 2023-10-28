c4bac166f6ea2aee8db8c24c5c0f1168faee6c16
apple <apple@apples-MacBook-Air.local>
2023-10-28 23:03:51 +0500

    Main - Project initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# SRD-Consulting React Project
+
+## Project Structure
+
+The project structure is organized as follows:
+
+
+- **public**: Contains the HTML entry point (`index.html`) and media assets in the `media` folder.
+
+- **src**: Contains React components in the `components` folder and application data in the `data` folder.
+
+- **App.css**: The main stylesheet for the project.
+
+- **App.jsx**: The entry point for the React application.
+
+- **.eslintrc**: ESLint configurations.
+
+- **.prettierrc**: Prettier configurations.
+
+- **package.json**: Manages project dependencies and scripts.
+
+## Getting Started
+
+1. Make sure you have Node.js and Yarn installed.
+
+2. Clone the repository.
+
+3. Install project dependencies:
+
+   ```bash
+    yarn install
+    yarn start
+
---
+++ src/components/navigation.jsx
@@ -0,0 +1,113 @@
+import React, {useState, useEffect} from "react";
+
+export const Navigation = (props) => {
+  const [scrollY, setScrollY] = useState(0);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [showTransparentNavbar, setShowTransparentNavbar] = useState(true)
+  const transparentScrollLimit = 100 // in pixels
+  const mobileScreenWidthLimit = 768 // in pixels
+
+  // Add a scroll event listener when the component mounts
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Remove the scroll event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (mobileScreenWidthLimit < screenWidth && scrollY <= transparentScrollLimit) {
+      setShowTransparentNavbar(true)
+    } else {
+      setShowTransparentNavbar(false)
+    }
+  }, [scrollY, screenWidth])
+
+  // Update screen width when the window is resized
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    // Attach the event listener when the component mounts
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); 
+
+  return (
+    <nav id="menu" className={`navbar navbar-default navbar-fixed-top ${showTransparentNavbar ? "navbar-transparent" : ""}`}>
+      <div className="container">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
+          >
+            {" "}
+            <span className="sr-only">Toggle navigation</span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+          </button>
+          <a className="navbar-brand page-scroll" href="#page-top">
+            SRD Consulting
+          </a>{" "}
+        </div>
+
+        <div
+          className="collapse navbar-collapse"
+          id="bs-example-navbar-collapse-1"
+        >
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a href="#features" className="page-scroll">
+                Features
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="page-scroll">
+                About
+              </a>
+            </li>
+            {/* <li>
+              <a href="#services" className="page-scroll">
+                Services
+              </a>
+            </li> */}
+            <li>
+              <a href="#portfolio" className="page-scroll">
+                Gallery
+              </a>
+            </li>
+            {/* <li>
+              <a href="#testimonials" className="page-scroll">
+                Testimonials
+              </a>
+            </li>
+            <li>
+              <a href="#team" className="page-scroll">
+                Team
+              </a>
+            </li> */}
+            <li>
+              <a href="#contact" className="page-scroll">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
